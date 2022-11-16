5f65674fa2cb19a52fabb716c1f85dffd1d250cc
gayathriganesan1996 <gayusharon96@gmail.com>
2022-11-16 12:04:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    DashboardComponent,
+    ProductsComponent,
+    StatisticsComponent,
+    CoupensComponent,
+    PagesComponent,
+    MediaComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { navbarData } from './nav-data';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  collapsed = false;
+  navData = navbarData;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
